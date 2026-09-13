3bc242a7ad2d92e9ef2b87f52d11df82ecb34cdf
bstydev <bastian.parra@alumnos.uv.cl>
2026-09-13 00:27:27 -0300

    add: se creó la base de los tipos que utilizaremos para trabajar de manera global en el control 1

---
+++ Controles/Control_1_Stream/src/types.ts
@@ -0,0 +1,37 @@
+// Aqui va la lógica de los nodos y la topologia que debe implementar el Lucas
+// básicamente debes construir el un modelo de datos validando la tabla de simbolos
+// te voy a dejar las interfaces básicas que menciona el profe creadas y luego tu creas la interfaz Operador que extiende de Nodo y el Grafo
+
+export enum TipoNodo {
+ FUENTE,
+ OPERADOR,
+ SUMIDEDO
+}
+
+export interface Nodo {
+ id: string;
+ tipo: TipoNodo;
+}
+
+export interface Operador extends Nodo {} // TODO
+export interface Grafo {} // TODO
+
+// Aqui va el contrato o lo que utiliza el Nico
+// (básicamente debes leer el archivo de txt usando ANTLR e invocar los metodos del Grafo que crea Lucas)
+
+// Esto es lo que necesito en el apartado de simulación:
+export interface Tupla {
+ idEvento: number;
+ trazaActual: string[]; // ejemplo de uso: ["FUENTE f1", "OPERADOR op1", "SUMIDERO s1"]
+ tiempoAcumulado: number; // suma de los tiempo de servicio
+}
+
+export interface Simulador {
+ // esto recibe el grafo que ya se construyo por Lucas y la cantidad de eventos extraída por el Nico
+ ejecutar(grafo: Grafo, cantidadEventos: number): void // ejecuta SIMULAR
+
+ // este metodo lo utilizo solo yo para la intruccion REPLICAS que se menciona
+ balancearCarga(operadorDestino: Operador): number;
+}
+
+// cuando este archivo
